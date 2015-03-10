4877d8e5a76c624e8422f04a7e3e662cb7ae4691
Joao Teodoro <joaotdn@gmail.com>
2015-03-10 10:51:20 -0300

    Adicionando classe Lista

---
+++ ListaSequencial/src/implementacao/Lista.java
@@ -0,0 +1,6 @@
+package implementacao;
+
+public class Lista {
+	Contato[] contatos = new Contato[100]; //vetor que armazena os contatos
+	int n  = 0; //posicao indice do ultimo elemento da lista
+}
